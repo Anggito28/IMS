70f4404f71aa5331c465fda89a045cfd209f97f5
Anggito28 <anggitosuryobaskoro286@gmail.com>
2023-07-31 12:36:33 +0700

    api update

--- control.php
+++ control.php
@@ -1,18 +1,35 @@
-<?php 
-if(isset($_POST['token'])){
+<?php
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    // Assuming the database connection is established in 'conection.php'
     include 'conection.php';
-    $key = $_POST['token'];
 
-    $sql = mysqli_query($dbconnect, "SELECT * FROM tb_kontrol WHERE token='$key'");
-    $query = mysqli_num_rows($sql);
+    // Read raw JSON data from the request body
+    $json_data = file_get_contents('php://input');
+    $data = json_decode($json_data, true);
 
-    if($query > 0){
-        $data = mysqli_fetch_assoc($sql);
-    }else {
-        $data = ["token" => "invalid"];
+    if ($data && isset($data['token'])) {
+        $key = $data['token'];
+
+        // Sanitize the input to prevent SQL injection (you can use prepared statements for better security)
+        $safe_key = mysqli_real_escape_string($dbconnect, $key);
+
+        $sql = mysqli_query($dbconnect, "SELECT * FROM tb_kontrol WHERE token='$safe_key'");
+        $query = mysqli_num_rows($sql);
+
+        if ($query > 0) {
+            $data = mysqli_fetch_assoc($sql);
+            $response = ["status" => "success", "data" => $data];
+        } else {
+            $response = ["status" => "error", "message" => "Token not found"];
+        }
+
+        // Convert the response array to JSON format
+        header('Content-Type: application/json');
+        echo json_encode($response);
+    } else {
+        $response = ["status" => "error", "message" => "Invalid or missing token"];
+        header('Content-Type: application/json');
+        echo json_encode($response);
     }
-    $res = json_encode($data);
-    echo $res;
 }
-
 ?>
